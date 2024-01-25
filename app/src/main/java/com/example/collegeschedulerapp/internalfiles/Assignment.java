f058d12230943b763bb1efee43df77bb1b2d5550
Brian P <bpark4684@gmail.com>
2024-01-25 18:46:48 -0500

    Added Assignment and Course java classes

---
+++ app/src/main/java/com/example/collegeschedulerapp/internalfiles/Assignment.java
@@ -0,0 +1,17 @@
+package com.example.collegeschedulerapp.internalfiles;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Date;
+
+public class Assignment {
+    String name;
+    LocalDate dueDate;
+    LocalDate dateAssigned;
+    LocalTime dueTime;
+
+    public Assignment(String name, LocalDate dueData, LocalDate dateAssigned, LocalTime dueTime) {
+
+    }
+}
+
---
+++ app/src/main/java/com/example/collegeschedulerapp/internalfiles/Course.java
@@ -0,0 +1,15 @@
+package com.example.collegeschedulerapp.internalfiles;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+    String name;
+    String instructor;
+
+    HashMap<String,String> meetingTime;
+
+    String room;
+    String location;
+
+}
